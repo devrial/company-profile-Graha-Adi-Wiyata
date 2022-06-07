c677d75d52bf5d9f1ba0fb58e2c51ccf1a9cdaf2
MUHAMMAD SAHRIAL <dev.sahrial@gmail.com>
2022-06-07 16:01:16 +0700

    update value span

--- company-profile/src/pages/Home/Home.js
+++ company-profile/src/pages/Home/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     <div className="grid grid-nogutter surface-0 text-800">
       <div className="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center ">
         <section>
-          <span className="block text-6xl font-bold mb-1">Create the screens your</span>
+          <span className="block text-6xl font-bold mb-1">Create the screens your-test</span>
           <div className="text-6xl text-primary font-bold mb-3">your visitors deserve to see</div>
           <p className="mt-0 mb-4 text-700 line-height-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
