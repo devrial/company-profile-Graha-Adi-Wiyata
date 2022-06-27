c14d64c6dc2e323c02212f2c6ada77fb3e010fbe
MUHAMMAD SAHRIAL <dev.sahrial@gmail.com>
2022-06-27 11:45:21 +0700

    add new css class and continuing develop contact us

--- company-profile/src/pages/Contact/Contact.js
+++ company-profile/src/pages/Contact/Contact.js
@@ -6,16 +6,41 @@ import "./styles.css"
 
 const Contact = () => {
   return (
-    <div className="lp-container">
-      <div className="lp-content-left">
-        <div className="lp-cl-container">
-          <div className="lp-cl-content">
-           <h1 className="titleHeader">Ada yang bisa kami bantu?</h1>
-           <span>tes</span>
+
+    <div className="layout-wrapper">
+      <div className="layout-main" style={{ marginLeft: 'auto' }}>
+        <div className="layout-content">
+          <div className="content-top">
+            <h1 className="titleHeader">Ada yang bisa kami bantu?</h1>
+            <span className="p-text-bold">CONSULTANT CONSTRUCTOR</span>
+          </div>
+          <div className="p-mt-3 container">
+            <div className="p-grid p-justify-between">
+              <div className="p-col-4">
+                <div className="box">2</div>
+              </div>
+              <div className="p-col-4">
+                <div className="box">4</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
+
+    // <div className="lp-container">
+    //   <div className="lp-content-left">
+    //     <div className="lp-cl-container">
+    //       <div className="lp-cl-content p-mt-2">
+    //         <h1 className="titleHeader">Ada yang bisa kami bantu?</h1>
+    //         <span className="p-text-bold">CONSULTANT CONSTRUCTOR</span>
+    //       </div>
+    //     </div>
+    //     <div className="container">
+    //       <p>tes</p>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
