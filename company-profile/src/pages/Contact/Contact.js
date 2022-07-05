c4d7f988571cfe3a46d3678944381c83750551b4
MUHAMMAD SAHRIAL <dev.sahrial@gmail.com>
2022-07-05 22:11:17 +0700

    continuing develop contact us screen

--- company-profile/src/pages/Contact/Contact.js
+++ company-profile/src/pages/Contact/Contact.js
@@ -8,44 +8,41 @@ const Contact = () => {
   return (
 
     <div className="layout-wrapper">
-      <div className="layout-main" style={{ marginLeft: 'auto' }}>
+      <div className="layout-main">
         <div className="layout-content">
           <div className="content-top">
-            <h1 className="titleHeader">Ada yang bisa kami bantu?</h1>
-            <span className="p-text-bold">CONSULTANT CONSTRUCTOR</span>
-          </div>
-          <div className="p-mt-3 container">
-            <div className="p-grid p-justify-between">
-              <div className="p-col-4">
-                <div className="box">
-                  <p>BCA Digital</p>
-                  <p>Terima kasih sudah mampir! Jika kamu memiliki pertanyaan seputar BCA Digital, hubungi kami melalui kontak di laman ini.</p>
-                </div>
-              </div>
-              <div className="p-col-4">
-                <div className="box">
-                  <p>Kantor Pusat BCA Digital</p>
-                  <p>The City Tower Lantai 11,
-                    Jl. M.H. Thamrin No.81,
-                    Jakarta Pusat, Indonesia 10310</p>
-                </div>
-              </div>
-            </div>
+            <h1 className="titleHeader">Contact Us</h1>
+            <h4 className="p-text-bold">Question not answered yet? We are here to help!</h4>
           </div>
         </div>
       </div>
     </div>
 
-    // <div className="lp-container">
-    //   <div className="lp-content-left">
-    //     <div className="lp-cl-container">
-    //       <div className="lp-cl-content p-mt-2">
+    // <div className="layout-wrapper">
+    //   <div className="layout-main" style={{ marginLeft: 'auto' }}>
+    //     <div className="layout-content">
+    //       <div className="content-top">
     //         <h1 className="titleHeader">Ada yang bisa kami bantu?</h1>
     //         <span className="p-text-bold">CONSULTANT CONSTRUCTOR</span>
     //       </div>
-    //     </div>
-    //     <div className="container">
-    //       <p>tes</p>
+    //       <div className="p-mt-3 container">
+    //         <div className="p-grid p-justify-between">
+    //           <div className="p-col-4">
+    //             <div className="box">
+    //               <p>BCA Digital</p>
+    //               <p>Terima kasih sudah mampir! Jika kamu memiliki pertanyaan seputar BCA Digital, hubungi kami melalui kontak di laman ini.</p>
+    //             </div>
+    //           </div>
+    //           <div className="p-col-4">
+    //             <div className="box">
+    //               <p>Kantor Pusat BCA Digital</p>
+    //               <p>The City Tower Lantai 11,
+    //                 Jl. M.H. Thamrin No.81,
+    //                 Jakarta Pusat, Indonesia 10310</p>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
     //     </div>
     //   </div>
     // </div>
